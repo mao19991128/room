cef53c554a0507f0a74ae53a4d93433382e72c03
mao19991128 <mao19991128@icloud.com>
2023-03-14 11:16:34 +0900

    人数が０で登録されないように修正、ヘッダーのデザインを修正

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,7 +4,7 @@ class Reservation < ApplicationRecord
 
     validates :check_in, presence: true
     validates :check_out, presence: true
-    validates :number, presence: true
+    validates :number, presence: true, numericality: {only_integer: true, greater_than: 0}
     validate :in_out_check
 
     def in_out_check
